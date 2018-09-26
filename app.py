6f2148c309355e8b06734db9d2aed4252be7d5d2
chenbo <1126531273@qq.com>
2018-09-26 12:49:39 +0800

    部署到github

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, Response, request
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+
+from config import pgsql_config
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = pgsql_config['url']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# app.register_blueprint()
+# app.add_url_rule("/login", view_func=login, methods=["POST"])
+
+
+# 测试用例
+@app.route('/', methods=["GET"])
+def hello_world():
+    return "nice to meet you!"
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    x = 0
+    username = request.form['username']
+    password = request.form['password']
+    userz = User.query.filter(User.username == username).first()
+    print(userz)
+    if userz:
+        if userz.password == password:
+            x = 1
+        else:
+            x = 0
+    user = User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return str(x)
+
+
+class User(db.Model):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True, autoincrement=True)  # 主键ID
+    username = Column(String(length=255))  # 用户名
+    password = Column(String(length=255))  # 密码
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
---
+++ config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+pgsql_config = {
+    "url": "postgresql+psycopg2://postgres:chenbo@localhost:5432/postgres"
+}
