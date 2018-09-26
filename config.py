7dbc2928dd0cbcf5aa077e5d9a5c1b8a05dbac9a
chenbo <1126531273@qq.com>
2018-09-26 17:05:17 +0800

    修改config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 pgsql_config = {
+    # "url": "postgresql+psycopg2://postgres:chenbo@localhost:5432/postgres"
     "url": "postgresql+psycopg2://crwnxksgnjzdmb" +
            ":ca9f437b29acf01f44aa6ad92a56ceae280976c7efaf7b5ed661473b246fb76e" +
            "@ec2-54-83-29-34.compute-1.amazonaws.com:5432/d3pi61guv6l2le"
